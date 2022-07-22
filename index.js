34ab595aed451fd2bfd80902d964938bc5fea3da
Michael Franco <franco.luis.michael@gmail.com>
2022-07-22 12:36:50 -0500

    commit tree 911624b989dff0095112d8d7864e2f3ee5eaeab7

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+// const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+
+// let totalBatteries = 0
+
+// batteryBatches.reduce(function(totalBatteries, currentValue) {
+//     return totalBatteries + currentValue
+// })
+
+
+// function totalBatteries(array){
+//     const total = array.reduce(function(accumulator, currentValue){
+//        return accumulator + currentValue
+//     }, 0)
+//     return total
+// };
+
+// const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+
+// // 0 + 1 + 2 + 3 + 4
+// const initialValue = 0;
+
+// const totalBatteries = batteryBatches.reduce(
+//   (previousValue, currentValue) => previousValue + currentValue,
+//   totalBatteries
+// );
+
+// console.log(totalBatteries);
+
+
+const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+  
+  function totalBatteries(batteries) {
+    let total = 0;
+  
+    for (const battery of batteries) {
+      total += battery;
+    };
+  
+    return total;
+  }
+  
+  console.log(totalBatteries(batteries));
